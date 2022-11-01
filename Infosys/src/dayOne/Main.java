752a5755ffeee1edf86ba242d36ae73435495078
Gla <Gla@192.168.1.39>
2022-11-01 20:23:43 +0530

    commit two

---
+++ Infosys/src/dayOne/Main.java
@@ -0,0 +1,14 @@
+package dayOne;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
